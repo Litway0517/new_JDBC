c27ab9b9f5fbb88bfe021bf1d4ddcaed69251090
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-09 22:23:11 +0800

    ConnectionTest 1.1

--- JDBC_1009/src/com/atguigu/java1/connection/ConnectionTest.java
+++ JDBC_1009/src/com/atguigu/java1/connection/ConnectionTest.java
@@ -42,6 +42,65 @@ public class ConnectionTest {
     }
 
 
+    // 方式二 -> 面向接口编程, 使用反射实现Driver的实例化. 这样第三方的代码就只有一个字符串了
+    @Test
+    public void testConnection2() throws ClassNotFoundException, InstantiationException,
+            IllegalAccessException, SQLException {
+
+        // 提供mysql中Driver接口的实现类对象
+        String className = "com.mysql.jdbc.Driver";
+        Class<?> clazz = Class.forName(className);
+        Driver driver = (Driver) clazz.newInstance();
+
+        // 注册驱动
+        DriverManager.registerDriver(driver);
+
+        // 连接基本信息
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "root";
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+        System.out.println(conn);
+
+        // 断言
+        Assert.assertNotNull(conn);
+
+    }
+
+
+    // 方式三 -> 省略注册的过程
+    @Test
+    public void testConnection3() throws SQLException, ClassNotFoundException {
+
+        //1. 获取连接的4个基本信息
+        String className = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/test";  //test:表示具体的数据库名
+        String user = "root";
+        String password = "root";
+
+
+        //2. 加载驱动
+        Class.forName(className);
+
+        //注册驱动
+//        DriverManager.registerDriver(driver);
+        /*
+        * 之所以不再代码中显式使用DriverManager的注册功能，是因为在msyql的Driver类的源码中发现：
+        *  static {
+                try {
+                    java.sql.DriverManager.registerDriver(new Driver());
+                } catch (SQLException E) {
+                    throw new RuntimeException("Can't register driver!");
+                }
+            }
+        *
+        * */
+        //3. 获取连接
+        Connection connection = DriverManager.getConnection(url, user, password);
+        System.out.println(connection);
+
+    }
 
 
 
