bb178de8657fc0e2d614e575accd72b626aaaffc
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-09 22:07:57 +0800

    java1 1.0

--- JDBC_1009/src/com/atguigu/java1/connection/ConnectionTest.java
+++ JDBC_1009/src/com/atguigu/java1/connection/ConnectionTest.java
@@ -1,6 +1,14 @@
 package com.atguigu.java1.connection;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * 连接测试
  *
@@ -9,6 +17,31 @@ package com.atguigu.java1.connection;
  */
 
 public class ConnectionTest {
+
+    @Test
+    public void testConnection1() throws SQLException {
+
+
+        // 提供mysql中的driver接口类实现
+        Driver driver = new com.mysql.jdbc.Driver();
+
+
+        // 注册驱动
+        DriverManager.registerDriver(driver);
+
+        // 连接基本信息
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "root";
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+        System.out.println(conn);
+
+        // 断言
+        Assert.assertNotNull(conn);
+    }
+
+
 
 
 
