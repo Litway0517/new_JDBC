24ceb6f6c5efb7273793b4b3d5a7115cd736f850
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-10 14:24:32 +0800

    封装的druid获取连接类 -> JDBCUtils 1.2

--- JDBC_1009/src/com/atguigu/java1/util/JDBCUtils.java
+++ JDBC_1009/src/com/atguigu/java1/util/JDBCUtils.java
@@ -21,20 +21,42 @@ import java.util.Properties;
  */
 public class JDBCUtils {
 
-    public static Connection getConnection() throws Exception {
-        // 创建prop对象
-        Properties prop = new Properties();
+    // 定义一个类的静态变量
+    /*
+        因为下面的静态方法, static初始化方法, 需要用到, dataSource这个变量, 而静态方法只能调用静态的对象.
+        因此, dataSource需要用static修饰
 
-        // 读取配置文件
-        InputStream fr = ClassLoader.getSystemClassLoader().getResourceAsStream("druid.properties");
-        prop.load(fr);
+     */
+    private static DataSource dataSource;
 
-        // 通过DruidDataSourceFactory创建连接池
-        DataSource source = DruidDataSourceFactory.createDataSource(prop);
+    /*
+        static代码块, 是当类加载完成的时候, 仅仅执行一次, 这样就只会有一个dataSource, 也就是只有一个数据池
+     */
+    static {
+        try {
+            // 创建prop对象
+            Properties prop = new Properties();
+            // 读取配置文件
+            InputStream fr = ClassLoader.getSystemClassLoader().getResourceAsStream("druid.properties");
+            prop.load(fr);
 
-        // 获取一个连接
-        return source.getConnection();
+            dataSource = DruidDataSourceFactory.createDataSource(prop);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    /*
+        每次调用这个方法, 仅仅是从数据库连接池中拿一个连接使用.
+        而不是像之前的方法, 每次都创建一个数据池, 再从数据池中拿一个连接(这种方式更慢)
+     */
+    public static Connection getConnection() throws Exception {
+        // 获取一个连接
+        return dataSource.getConnection();
+    }
+
+
+
 
 }
 
