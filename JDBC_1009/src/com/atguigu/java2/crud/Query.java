16045d843a5b39696f41c9399b50f23368ceba10
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-12 14:57:20 +0800

    CURD操作之查询操作 -> Query 1.1

--- JDBC_1009/src/com/atguigu/java2/crud/Query.java
+++ JDBC_1009/src/com/atguigu/java2/crud/Query.java
@@ -7,10 +7,13 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 查询
@@ -65,7 +68,6 @@ public class Query {
     /*
         BeanListHandler -> 查询, 对应表中的多条数据. 以以对象构成集合的方式返回
      */
-
     @Test
     public void testQuery2() {
         Connection conn = null;
@@ -93,6 +95,78 @@ public class Query {
         }
 
     }
+
+
+    /*
+        使用MapHandler存储查询到的 一条 数据
+            这时返回的就是一个集合对象, 这个集合中有键值对
+
+        map中的key为表的字段名, map中的value为表的一条数据的数据值.
+     */
+    @Test
+    public void testQuery3() {
+        Connection conn = null;
+
+        try {
+            // 1. 获取数据库的连接
+            conn = JDBCUtils.getConnection();
+
+            // 2. 提供一条带有占位符的查询语句
+            String sql = "SELECT id,name,email,birth FROM customers where id = ?";
+
+            // 3. 创建QueryRunner实例
+            QueryRunner runner = new QueryRunner();
+
+            // 4. 通过QueryRunner的实例, 调用其query()方法, 查询. 会返回一个结果集(这个结果就是java对象了)
+            MapHandler mapHandler = new MapHandler();
+            Map<String, Object> map = runner.query(conn, sql, mapHandler, 1);
+            System.out.println(map);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 5. 关闭资源
+            JDBCUtils.close(conn);
+        }
+
+    }
+
+    /*
+        使用MapListHandler存储查询到的 多条 数据
+            这时返回的就是一个大集合, 实际上是一个list, 这个list的每个元素是一个一个的小集合,
+            集合中有键值对. 就和上面一样了.
+     */
+    @Test
+    public void testQuery4() {
+        Connection conn = null;
+
+        try {
+            // 1. 获取数据库的连接
+            conn = JDBCUtils.getConnection();
+
+            // 2. 提供一条带有占位符的查询语句
+            String sql = "SELECT id,name,email,birth FROM customers where id > ?";
+
+            // 3. 创建QueryRunner实例
+            QueryRunner runner = new QueryRunner();
+
+            // 4. 通过QueryRunner的实例, 调用其query()方法, 查询. 会返回一个结果集(这个结果就是java对象了)
+            MapListHandler mapList = new MapListHandler();
+            List<Map<String, Object>> map = runner.query(conn, sql, mapList, 1);
+
+            map.forEach(System.out::println);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 5. 关闭资源
+            JDBCUtils.close(conn);
+        }
+
+    }
+
+
+
 }
 
 
