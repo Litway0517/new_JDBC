1413fc0213c44f88c9dbe5a2f5758f6dde8626f5
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-13 22:26:36 +0800

    数据库事务的讲解 -> Transaction 1.1

--- JDBC_1009/src/com/atguigu/java4/Transaction.java
+++ JDBC_1009/src/com/atguigu/java4/Transaction.java
@@ -1,6 +1,13 @@
 package com.atguigu.java4;
 
 
+import com.atguigu.java1.util.JDBCUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * 事务
  *
@@ -29,5 +36,52 @@ public class Transaction {
         操作1 -> UPDATE user_table SET balance = balance - 100 WHERE user = 'AA';
         操作2 -> UPDATE user_table SET balance = balance - 100 WHERE user = 'BB';
      */
+
+    @Test
+    public void test1() {
+
+        /*
+            下面代码的执行过程 ->
+                当执行完sql1的时候, 就会继续执行sout语句, 但是被除数是0, 显然是异常, 因此代码转入到
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                最后执行 ->
+                finally {
+                    JDBCUtils.close(conn);
+                }
+                关闭了资源就表明, 会提交, 但是AA转账了100元, BB并没有收到啊? 钱没了!!
+
+                看test2
+         */
+
+        Connection conn = null;
+        try {
+            QueryRunner runner = new QueryRunner();
+            conn = JDBCUtils.getConnection();
+
+
+            // 操作1
+            String sql1 = "UPDATE user_table SET balance = balance - 100 WHERE user = ?;";
+            int aa = runner.update(conn, sql1, "AA");
+
+            // 模拟异常出现
+            System.out.println(10 / 0);
+
+
+            // 操作2
+            String sql2 = "UPDATE user_table SET balance = balance + 100 WHERE user = ?;";
+            int bb = runner.update(conn, sql2, "BB");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.close(conn);
+        }
+
+
+
+    }
+
+
 
 }
